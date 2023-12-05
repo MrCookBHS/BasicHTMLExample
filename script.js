1ed8931474b05dbe670245b98ef92f14ef505dbf
Andrew Cook <133822242+MrCookBHS@users.noreply.github.com>
2023-12-05 08:50:24 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Keeping track of the number of total clicks
+var count = 0;
+
+// Getting the screen elements in variables
+var countButton = document.getElementById("countButton");
+var counterLabel = document.getElementById("counterLabel");
+var winLabel = document.getElementById("winLabel");
+var myImage = document.getElementById("goodJobImage");
+
+// OnEvent for the button
+countButton.onclick = function() {
+  console.log("You pressed the button");
+  myImage.style.display = "block"; // Makes the image appear
+  count++;
+  counterLabel.innerHTML = "Total count: " + count;
+  if(count==5) {
+    winLabel.innerHTML = "YOU WIN!";
+  }
+  if(count==10) {
+    winLabel.innerHTML = "You already won, stop it";
+    winLabel.style.color = "blue";
+  }
+}
